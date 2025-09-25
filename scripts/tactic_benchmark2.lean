7b3182007e8b13b2b582c2ae844421d8fe225dc3
JOSHCLUNE <josh.seth.clune@gmail.com>
2025-09-25 00:30:56 -0400

    tactic_benchmark2 bug fix

--- scripts/tactic_benchmark2.lean
+++ scripts/tactic_benchmark2.lean
@@ -333,7 +333,7 @@ def runLeanSMTAtAliasDecl (mod : Name) (declName : Name) (decls : ConstantInfo �
     for jsonEntry in json do
       let jsonDeclName ← IO.ofExcept $ jsonEntry.getObjVal? "declName"
       let curDeclName ← IO.ofExcept $ jsonDeclName.getStr?
-      if curDeclName == s!"{ci.name}" then
+      if s!"{curDeclName}__eval" == s!"{ci.name}" then
         ciEntry := jsonEntry
         dbg_trace "Found jsonEntry for {declName}"
         break
