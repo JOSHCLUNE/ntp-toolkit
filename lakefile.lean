cbcccaa82e6a152de6d89232d4f812c4469a7078
Ubuntu <ubuntu@ip-172-31-2-2.us-east-2.compute.internal>
2026-01-01 03:54:38 +0000

    Update querySMT

--- lakefile.lean
+++ lakefile.lean
@@ -16,7 +16,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.22.0"
 
 require QuerySMT from git
-  "https://github.com/JOSHCLUNE/LeanSMTParser.git" @ "main"
+  "https://github.com/JOSHCLUNE/LeanSMTParser.git" @ "b694a648c953456cece1771d0589e5278fb0421f"
 
 -- require smt from git "https://github.com/ufmg-smite/lean-smt.git" @ "a79af6cf74b9c4ad3bfb755813fa856f7f41fa9e"
 
